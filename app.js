9d507a95d90a063e911d3f7e154541c5b6c5bb70
young89-1 <mny76ers@yahoo.com>
2023-09-05 19:55:09 -0400

    added mongoDB and backend

--- app.js
+++ app.js
@@ -5,23 +5,46 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+const client = require('./config/database');
+const mongoose = require('mongoose');
+const { Schema } = mongoose.Schema;
+const cors = require('cors')
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+mongoose.connect('mongodb+srv://young89:mny76ers@cluster0.cvee3vd.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(logger('dev'));
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// const apiRouter = require('./routes/index');
+// app.use('/index', apiRouter)
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'jsx');
+// app.engine('jsx', require('express-react-views').createEngine());
+
+
+app.use('/', indexRouter);
+
+app.listen(27017, function(error){
+  if(error) throw error
+  console.log("Server created Successfully")
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -33,9 +56,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
---
+++ config/database.js
@@ -0,0 +1,13 @@
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://young89:mny76ers@cluster0.cvee3vd.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// module.exports = client;
