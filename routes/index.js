e5744e07de9fa30fa51f4e1712c0253b7bfb8b52
young89-1 <mny76ers@yahoo.com>
2023-09-06 20:10:11 -0400

    added backend register functionality

--- routes/index.js
+++ routes/index.js
@@ -2,41 +2,59 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/User')
 
-router.post('/resources', (req, res) => {
-  res.json({ success: true });
-})
-
-// let dave = new User({Username: 'Dave', Tweets: [1,3,5], Friends: ['bob', 'jannette', 'brad']})
-// dave.save();
-// console.log(dave)
-
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/api/chirps', function(req, res, next) {
 });
 
-router.get('/register', function (req, res)  {
+router.post('/api/chirps', function(req, res, next) {
 });
 
-router.post('/register', async (req, res) => {
+router.get('/api/register', function (req, res)  {
+});
+
+router.post('/api/register', async (req, res) => {
+  const { firstName, lastName, Username, Password, Email } = req.body;
   try {
-    const newUser = new User({
-    Username: 'Dave', 
-    Password: 'Davepass',
-    Email: 'dave@mail.com',
-    Tweets: [1,3,5], 
-    Friends: ['bob', 'jannette', 'brad']
-  });
-    console.log(newUser)
-
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    const existingUser = await User.findOne({ Username });
+    console.log("existingUser", existingUser);
+    if (existingUser) {
+      return res.status(400).send("Username already taken.");
     }
+    const newUser = new User({
+      firstName,
+      lastName,
+      Username,
+      Password,
+      Email,
+      Chirp: [],
+      Friends: [],
+    });
+
+    await newUser.save();
+
+    res.redirect("api/login");
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send("An error occurred while creating the user.");
+  }
 })
 
-router.get('')
+router.get('api/login', function(req, res, next) {
+});
+
+router.post('api/login', function(req, res, next) {
+});
+
+router.get('api/profile', function(req, res, next) {
+});
+
+router.post('api/profile', function(req, res, next) {
+});
+
+router.get('api/friends', function(req, res, next) {
+});
+
+router.post('api/friends', function(req, res, next) {
+});
 
 module.exports = router;
